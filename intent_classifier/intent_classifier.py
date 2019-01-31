570de4fae69f6716b6ebbb1fdf88b30dd5d6f9ef
Pavel <pavilbezpravil@gmail.com>
2019-01-31 22:53:38 +0700

    add naive bayes

--- intent_classifier/intent_classifier.py
+++ intent_classifier/intent_classifier.py
@@ -6,18 +6,19 @@ License: Apache License 2.0.
 
 """
 
-import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+
+from .NaiveBayesIntentClassifier import NaiveBayesIntentClassifier
 
 
 class IntentClassifier(BaseEstimator, ClassifierMixin):
-    """ Sequence-to-sequence classifier, which converts one language sequence into another. """
+    """ IntentClassifier base class. """
 
-    def __init__(self):
-        """ Create a new object with specified parameters.
+    def __init__(self, method_name):
+        if method_name == 'naive bayes':
+            return NaiveBayesIntentClassifier()
 
-        """
-        pass
+        raise NotImplementedError(method_name)
 
     def fit(self, X, y, **kwargs):
         """
@@ -31,51 +32,3 @@ class IntentClassifier(BaseEstimator, ClassifierMixin):
 
     def fit_predict(self, X, y, **kwargs):
         return None
-
-    def load_weights(self, weights_as_bytes):
-        """
-        """
-        pass
-
-    def dump_weights(self):
-        """
-        """
-        pass
-
-    def get_params(self, deep=True):
-        """
-        """
-        pass
-
-    def set_params(self, **params):
-        """
-        """
-        pass
-
-    def dump_all(self):
-        """
-        """
-        pass
-
-    def load_all(self, new_params):
-        """
-        """
-        pass
-
-    @staticmethod
-    def check_params(**kwargs):
-        """
-        """
-        pass
-
-    @staticmethod
-    def check_X(X, checked_object_name=u'X'):
-        """
-        """
-        pass
-
-    @staticmethod
-    def tokenize_text(src, lowercase):
-        """
-        """
-        return list(filter(lambda it: len(it) > 0, src.strip().lower().split() if lowercase else src.strip().split()))
