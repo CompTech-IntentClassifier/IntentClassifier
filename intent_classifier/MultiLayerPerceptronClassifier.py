46bc91ac79f7af0397974ab94d2dff2d2bfaffab
Daniil Yakovlev <36961516+ddyak@users.noreply.github.com>
2019-02-02 16:31:50 +0700

    Update MultiLayerPerceptronClassifier.py

--- intent_classifier/MultiLayerPerceptronClassifier.py
+++ intent_classifier/MultiLayerPerceptronClassifier.py
@@ -2,26 +2,35 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer 
 
 
+class MultiLayerPerceptronClassifier(BaseEstimator, ClassifierMixin):
+    """ Perceptron classifier, which determines the user's intent. """
+    def __init__(self, hiddien_layer=80, neuron=80):    
+        """ Create a new object """
+        self.feature_extraction = TfidfVectorizer()   
+        self.clf = MLPClassifier(hidden_layer_sizes=(neuron,hiddien_layer),activation='relu', solver='adam', random_state=42)
 
-
-class MultiLayerPerceptronClassifier(MLPClassifier, TfidfVectorizer):
-	"""docstring for MultiLayerPerceptronClassifier"""
-	def __init__(self, hiddien_layer = 80, neuron=80):
-		self.feature_extraction = TfidfVectorizer()   
-		self.clf = MLPClassifier(hidden_layer_sizes=(neuron,hiddien_layer),activation='relu', solver='adam', random_state=42)
-
-	def fit(self, X, y, **kwargs):
-		self.tfidf = self.feature_extraction.fit(X)
-		X_train = self.tfidf.transform(X)
-		self.clf.fit(X_train, y)
-		return self
-
+    def fit(self, X, y, **kwargs):
+        """ Fit the perceptron to convert sequence to intent.
+        
+        :param X: input texts for training.
+        :param y: target intents for training.
+        
+        :return self
+        
+        """
+        self.tfidf = self.feature_extraction.fit(X)
+        X_train = self.tfidf.transform(X)
+        self.clf.fit(X_train, y)
+        return self
 
 	def predict(self, X):
-		print(dir(self))
-		X_train = self.tfidf.transform([X])
-		return self.clf.predict(X_train)
-
-		
-	def fit_predict(self, X, y, **kwargs):
-		return self.fit(X, y).predict(X)
+        """ Predict resulting intents by source sequences with a trained logistic regression model.
+        
+        :param X: source sequences.
+        
+        :return: resulting intents, predicted for source sequences.
+        
+        """
+        X_train = self.tfidf.transform([X])
+        return self.clf.predict(X_train)
+    
